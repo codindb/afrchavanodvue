4675ddc193cd486b4cf44725707c3750c7082bba
codindb <codindb.dev@gmail.com>
2021-08-13 18:07:32 +0200

    wip: news carousel

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,47 +1,23 @@
 <template>
    <div>
-      <h2>Home</h2>
-      <el-button @click="sendNotification" size="small" type="primary" icon="el-icon-news" circle></el-button>
-      <el-button @click="loadNews" :loading="strapiData.areNewsLoading" size="small" type="success" icon="el-icon-download" circle></el-button>
-      <img v-for="news in strapiData.news" :src="news.photo.url" :alt="news.photo.url">
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam porro, doloremque veritatis nam modi adipisci optio perferendis totam eligendi iure autem voluptatum quia sapiente debitis sit quidem nisi praesentium recusandae?</p>
-      <p>Adipisci et repellat, id quae aliquid eaque quia facere ipsam eveniet architecto, cumque debitis maxime dolores harum error saepe tempore nobis! Assumenda molestiae earum pariatur veritatis possimus eius autem dolor.</p>
-      <p>Quos quia optio minima quam libero quo ad illo necessitatibus magnam nisi laborum corporis nemo in nihil saepe voluptatum laboriosam voluptate qui tenetur labore delectus, sequi expedita. Praesentium, deleniti nobis.</p>
-      <p>Dolores culpa eos aspernatur libero recusandae necessitatibus autem, non mollitia rerum sunt sed ipsa totam modi! Optio error pariatur repudiandae, mollitia provident necessitatibus? Ullam, ex quam unde aliquid molestiae reiciendis?</p>
-      <p>Tempora reprehenderit deleniti voluptas obcaecati fugiat eum laboriosam rem, aliquam ut odit magni enim voluptatum dolore voluptates in quidem cum. Aperiam, in. Cupiditate autem laborum nihil odio. Aliquid, maiores cupiditate.</p>
-      <p>Voluptatibus sit ratione, voluptatum iste molestiae quos eaque, praesentium corporis quas ipsum sint id, nostrum similique veniam molestias. Incidunt excepturi similique exercitationem reiciendis expedita alias vero quam porro obcaecati consequatur!</p>
-      <p>Aliquam pariatur animi porro distinctio voluptatem officiis nemo illum, quod sequi nobis voluptatum dolor ducimus. Hic labore porro, minima est magni placeat omnis repellat tempore itaque consectetur aliquam maxime sit.</p>
-      <p>Facilis incidunt, doloribus ducimus, porro consectetur tenetur doloremque maxime, delectus adipisci aliquam repudiandae fuga amet quia exercitationem et. Temporibus eveniet voluptate, blanditiis beatae sed libero a sint quo dolorem illum.</p>
-      <p>Illo ipsa quas sint nam doloribus iste ipsum culpa, delectus illum eum. Velit eveniet aliquam tempore voluptatem sapiente numquam nostrum consectetur vero, soluta atque in deleniti, at, omnis ipsa cumque?</p>
-      <p>Vel magni accusamus eaque officia praesentium, officiis nostrum nemo! Et expedita mip>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam porro, doloremque veritatis nam modi adipisci optio perferendis totam eligendi iure autem voluptatum quia sapiente debitis sit quidem nisi praesentium recusandae?</p>
-      <p>Adipisci et repellat, id quae aliquid eaque quia facere ipsam eveniet architecto, cumque debitis maxime dolores harum error saepe tempore nobis! Assumenda molestiae earum pariatur veritatis possimus eius autem dolor.</p>
-      <p>Quos quia optio minima quam libero quo ad illo necessitatibus magnam nisi laborum corporis nemo in nihil saepe voluptatum laboriosam voluptate qui tenetur labore delectus, sequi expedita. Praesentium, deleniti nobis.</p>
-      <p>Dolores culpa eos aspernatur libero recusandae necessitatibus autem, non mollitia rerum sunt sed ipsa totam modi! Optio error pariatur repudiandae, mollitia provident necessitatibus? Ullam, ex quam unde aliquid molestiae reiciendis?</p>
-      <p>Tempora reprehenderit deleniti voluptas obcaecati fugiat eum laboriosam rem, aliquam ut odit magni enim voluptatum dolore voluptates in quidem cum. Aperiam, in. Cupiditate autem laborum nihil odio. Aliquid, maiores cupiditate.</p>
-      <p>Voluptatibus sit ratione, voluptatum iste molestiae quos eaque, praesentium corporis quas ipsum sint id, nostrum similique veniam molestias. Incidunt excepturi similique exercitationem reiciendis expedita alias vero quam porro obcaecati consequatur!</p>
-      <p>Aliquam pariatur animi porro distinctio voluptatem officiis nemo illum, quod sequi nobis voluptatum dolor ducimus. Hic labore porro, minima est magni placeat omnis repellat tempore itaque consectetur aliquam maxime sit.</p>
-      <p>Facilis incidunt, doloribus ducimus, porro consectetur tenetur doloremque maxime, delectus adipisci aliquam repudiandae fuga amet quia exercitationem et. Temporibus eveniet voluptate, blanditiis beatae sed libero a sint quo dolorem illum.</p>
-      <p>Illo ipsa quas sint nam doloribus iste ipsum culpa, delectus illum eum. Velit eveniet aliquam tempore voluptatem sapiente numquam nostrum consectetur vero, soluta atque in deleniti, at, omnis ipsa cumque?</p>
-      <p>Vel magni accusamus eaque officia praesentium, officiis nostrum nemo! Et expedita mip>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam porro, doloremque veritatis nam modi adipisci optio perferendis totam eligendi iure autem voluptatum quia sapiente debitis sit quidem nisi praesentium recusandae?</p>
-      <p>Adipisci et repellat, id quae aliquid eaque quia facere ipsam eveniet architecto, cumque debitis maxime dolores harum error saepe tempore nobis! Assumenda molestiae earum pariatur veritatis possimus eius autem dolor.</p>
-      <p>Quos quia optio minima quam libero quo ad illo necessitatibus magnam nisi laborum corporis nemo in nihil saepe voluptatum laboriosam voluptate qui tenetur labore delectus, sequi expedita. Praesentium, deleniti nobis.</p>
-      <p>Dolores culpa eos aspernatur libero recusandae necessitatibus autem, non mollitia rerum sunt sed ipsa totam modi! Optio error pariatur repudiandae, mollitia provident necessitatibus? Ullam, ex quam unde aliquid molestiae reiciendis?</p>
-      <p>Tempora reprehenderit deleniti voluptas obcaecati fugiat eum laboriosam rem, aliquam ut odit magni enim voluptatum dolore voluptates in quidem cum. Aperiam, in. Cupiditate autem laborum nihil odio. Aliquid, maiores cupiditate.</p>
-      <p>Voluptatibus sit ratione, voluptatum iste molestiae quos eaque, praesentium corporis quas ipsum sint id, nostrum similique veniam molestias. Incidunt excepturi similique exercitationem reiciendis expedita alias vero quam porro obcaecati consequatur!</p>
-      <p>Aliquam pariatur animi porro distinctio voluptatem officiis nemo illum, quod sequi nobis voluptatum dolor ducimus. Hic labore porro, minima est magni placeat omnis repellat tempore itaque consectetur aliquam maxime sit.</p>
-      <p>Facilis incidunt, doloribus ducimus, porro consectetur tenetur doloremque maxime, delectus adipisci aliquam repudiandae fuga amet quia exercitationem et. Temporibus eveniet voluptate, blanditiis beatae sed libero a sint quo dolorem illum.</p>
-      <p>Illo ipsa quas sint nam doloribus iste ipsum culpa, delectus illum eum. Velit eveniet aliquam tempore voluptatem sapiente numquam nostrum consectetur vero, soluta atque in deleniti, at, omnis ipsa cumque?</p>
-      <p>Vel magni accusamus eaque officia praesentium, officiis nostrum nemo! Et expedita mip>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam porro, doloremque veritatis nam modi adipisci optio perferendis totam eligendi iure autem voluptatum quia sapiente debitis sit quidem nisi praesentium recusandae?</p>
-      <p>Adipisci et repellat, id quae aliquid eaque quia facere ipsam eveniet architecto, cumque debitis maxime dolores harum error saepe tempore nobis! Assumenda molestiae earum pariatur veritatis possimus eius autem dolor.</p>
-      <p>Quos quia optio minima quam libero quo ad illo necessitatibus magnam nisi laborum corporis nemo in nihil saepe voluptatum laboriosam voluptate qui tenetur labore delectus, sequi expedita. Praesentium, deleniti nobis.</p>
-      <p>Dolores culpa eos aspernatur libero recusandae necessitatibus autem, non mollitia rerum sunt sed ipsa totam modi! Optio error pariatur repudiandae, mollitia provident necessitatibus? Ullam, ex quam unde aliquid molestiae reiciendis?</p>
-      <p>Tempora reprehenderit deleniti voluptas obcaecati fugiat eum laboriosam rem, aliquam ut odit magni enim voluptatum dolore voluptates in quidem cum. Aperiam, in. Cupiditate autem laborum nihil odio. Aliquid, maiores cupiditate.</p>
-      <p>Voluptatibus sit ratione, voluptatum iste molestiae quos eaque, praesentium corporis quas ipsum sint id, nostrum similique veniam molestias. Incidunt excepturi similique exercitationem reiciendis expedita alias vero quam porro obcaecati consequatur!</p>
-      <p>Aliquam pariatur animi porro distinctio voluptatem officiis nemo illum, quod sequi nobis voluptatum dolor ducimus. Hic labore porro, minima est magni placeat omnis repellat tempore itaque consectetur aliquam maxime sit.</p>
-      <p>Facilis incidunt, doloribus ducimus, porro consectetur tenetur doloremque maxime, delectus adipisci aliquam repudiandae fuga amet quia exercitationem et. Temporibus eveniet voluptate, blanditiis beatae sed libero a sint quo dolorem illum.</p>
-      <p>Illo ipsa quas sint nam doloribus iste ipsum culpa, delectus illum eum. Velit eveniet aliquam tempore voluptatem sapiente numquam nostrum consectetur vero, soluta atque in deleniti, at, omnis ipsa cumque?</p>
-      <p>Vel magni accusamus eaque officia praesentium, officiis nostrum nemo! Et expedita minima sapiente aliquid quis voluptatem aspernatur reprehenderit voluptate doloribus dolores asperiores id reiciendis voluptatibus exercitationem nihil ipsa, fuga quo!</p>
       <el-empty v-if="strapiData.areNewsLoading" description="Chargement..."></el-empty>
+      <el-carousel :initial-index="2" trigger="click" :interval="4000" type="card" height="200px">
+         <el-carousel-item v-for="item in strapiData.news" :key="item.id">
+            <div>
+               <img :src="item.photo.url" class="image">
+            </div>
+         </el-carousel-item>
+      </el-carousel>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi, consequuntur optio. Dicta velit voluptates suscipit magni consequatur ipsum, molestiae quis repudiandae quam debitis repellat eligendi exercitationem, ea impedit placeat perferendis.</p>
+      <p>Ipsum repudiandae natus suscipit itaque odio quod sit. Itaque debitis mollitia, nostrum corrupti, numquam vel libero praesentium ullam, laudantium soluta ex autem perferendis voluptates saepe iusto eius sunt delectus. Sed!</p>
+      <p>Architecto accusantium, odio tempora ab adipisci quae atque sed modi error doloribus possimus ducimus deleniti. Reprehenderit, minus est? Harum, sint delectus quas cumque eveniet nulla rem consectetur eius consequuntur voluptate?</p>
+      <p>Maiores facilis impedit omnis quas! Quisquam praesentium natus, nihil iure et harum necessitatibus, eius facilis exercitationem sapiente perspiciatis distinctio ut! Quibusdam unde architecto perspiciatis veniam, magni sint aliquam voluptatibus iure?</p>
+      <p>Illum fugiat, doloribus, soluta voluptatum quam illo porro id dolorem deserunt inventore ullam sequi, neque provident dignissimos? Adipisci a libero quisquam illo eos earum facere, unde expedita harum quis alias!</p>
+      <p>Blanditiis dicta beatae harum reiciendis eligendi, animi quia veniam odio placeat quisquam, odit numquam ad corporis, quas pariatur maiores tenetur dolore repellendus necessitatibus reprehenderit fugit sit consequatur? Reprehenderit, ex! Fuga!</p>
+      <p>Inventore quis nobis hic? Error, magnam id! Distinctio non quibusdam delectus exercitationem asperiores est assumenda similique minus. Et cum accusantium recusandae placeat error, sint quod sapiente quis porro eum ducimus?</p>
+      <p>Laudantium, modi voluptatum repellat esse debitis iste libero accusantium nulla corrupti quae tempora nemo molestiae. Earum ea similique reprehenderit neque, voluptates suscipit ullam, autem exercitationem ducimus alias illo eos numquam?</p>
+      <p>Earum aperiam repudiandae laudantium quae ratione pariatur, cumque quidem aspernatur modi sapiente nulla explicabo ducimus asperiores vel. Inventore consequuntur veniam eveniet nobis neque numquam accusamus. Incidunt dolores vel rem deleniti.</p>
+      <p>Vitae at et molestiae vero repellendus ipsum debitis maxime velit similique mollitia ut quia aliquam corrupti, nemo officia repellat tempore incidunt eveniet voluptate. Delectus quia sint laudantium fuga illo reprehenderit!</p>
    </div>
 </template>
 
@@ -67,12 +43,15 @@
          title: "Erreur",
          message: "Impossible de charger les données",
          duration: 5000
-      });
+         });
       }
    }
+   loadNews()
 
 </script>
 
 <style lang="scss" scoped>
+
+
 
 </style>
