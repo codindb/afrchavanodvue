4dbe42d1246cc43fd984571115358a425b36cf5d
codindb <codindb.dev@gmail.com>
2021-08-25 19:27:01 +0200

    Test: nocodb as API

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -10,14 +10,14 @@
 
       <el-carousel class="desktop-carousel" v-if="!displayMobile()" ref="carousel" arrow="always" trigger="click" :interval="4000" type="card" height="380px">
          <el-carousel-item v-for="item in apiData.news" :key="item">
-               <el-image :src="item.photo.url" fit="fill" class="image"></el-image>
+               <el-image :src="JSON.parse(item.photo)[0].url" fit="fill" class="image"></el-image>
                <div class="newsTitle"> {{ item.titre }}</div>
          </el-carousel-item>
       </el-carousel>
 
       <el-carousel class="mobile-carousel" v-if="displayMobile()" ref="carousel" indicator-position="outside" arrow="always" trigger="click" :interval="4000" height="250px">
          <el-carousel-item v-for="item in apiData.news" :key="item">
-               <el-image :src="item.photo.url" fit="fill" class="image"></el-image>
+               <el-image :src="item.photo" fit="fill" class="image"></el-image>
                <div class="newsTitle"> {{ item.titre }}</div>
          </el-carousel-item>
       </el-carousel>
--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -1,13 +1,15 @@
 import axios from "axios";
 
+const NOCODB_APIKEY = import.meta.env.VITE_NOCODB_APIKEY
+
 // axios instance definition
 const instance = axios.create({
-   baseURL: 'https://afrchavanod74.herokuapp.com',
-   headers: { 'Content-Type': 'application/json; charset=utf-8'}
+   baseURL: 'http://afrchavanodnocodb.herokuapp.com/nc/afrchavanod_fr_i5Eo/api/v1',
+   headers: { 'Content-Type': 'application/json; charset=utf-8', 'xc-auth': NOCODB_APIKEY}
 })
 
 // Get all News
 export async function getAllNews () {
-   const res = await instance.get('/actualites')
+   const res = await instance.get('/Actualites')
    return res.data
 }
