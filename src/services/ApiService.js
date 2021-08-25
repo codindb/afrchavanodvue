d10be655e7c91111e46c34e3fa42e0300e02be6a
codindb <codindb.dev@gmail.com>
2021-08-25 22:46:58 +0200

    Add: header for http request

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -4,8 +4,8 @@ const NOCODB_APIKEY = import.meta.env.VITE_NOCODB_APIKEY
 
 // axios instance definition
 const instance = axios.create({
-   baseURL: 'http://afrchavanodnocodb.herokuapp.com/nc/afrchavanod_fr_i5Eo/api/v1',
-   headers: { 'Content-Type': 'application/json; charset=utf-8', 'xc-auth': NOCODB_APIKEY}
+   baseURL: 'https://afrchavanodnocodb.herokuapp.com/nc/afrchavanod_fr_i5Eo/api/v1',
+   headers: { 'Content-Type': 'application/json; charset=utf-8', 'xc-auth': NOCODB_APIKEY, 'Upgrade-Insecure-Requests': 1}
 })
 
 // Get all News
