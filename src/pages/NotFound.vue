c0d9aa7d097f12d768fc5e4672c64371d46dee24
codindb <codindb.dev@gmail.com>
2021-08-26 16:11:05 +0200

    Add: elements to not found page

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,11 +1,30 @@
 <template>
-   <h2>NOT FOUND</h2>
+   <el-image :src="notFoundImageUrl"></el-image>
+   <h2>Page Introuvable 😵‍💫</h2>
+   <el-row justify="center">
+      <el-col @click="router.back()" :span="8" :sm="4"><h3>🔙</h3></el-col>
+      <el-col :span="8" :sm="4"><router-link :to="{ name: 'Home' }"><h3>🏠</h3></router-link></el-col>
+   </el-row>
+   <div class="bottomGap"></div>
 </template>
 
 <script setup>
+
+import { useRouter } from 'vue-router'
+
+// 404 image Url
+import notFoundImageUrl from '../assets/404.png'
+
+const router = useRouter()
    
 </script>
 
 <style lang="scss" scoped>
-
+   h2 {
+      font-size: 36px;
+   }
+   h3 {
+      font-size: 60px;
+      cursor: pointer;
+   }
 </style>
