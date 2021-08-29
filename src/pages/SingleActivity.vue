3bf1f3a9cbb94a06b701d0b4ea3f83047158f7a5
codindb <codindb.dev@gmail.com>
2021-08-29 02:29:07 +0200

    Add: content

--- src/pages/SingleActivity.vue
+++ src/pages/SingleActivity.vue
@@ -7,7 +7,16 @@
       <el-empty v-if="apiData.areActivitiesLoading" description="Chargement..."></el-empty>
       <el-main class="overlapping page-main" v-else>
          <el-image :src="apiData.singleActivity.photo.url"></el-image>
+         <h3>Description</h3>
          <p>{{ apiData.singleActivity.description_complete }}</p>
+         <h3>Horaires</h3>
+         <p>le Lundi de 12h à 14h</p>
+         <h3>Tarif</h3>
+         <p>100€ / trimestre</p>
+         <h3>Contact</h3>
+         <p><strong>Responsable d'activité: </strong>Toto</p>
+         <p><strong>E-mail: </strong>toto@afr.com</p>
+         <p><strong>Téléphone: </strong><a href="tel:+33 6 12 34 56 78">0612345678</a></p>
          <div class="bottomGap"></div>
       </el-main>
    </el-container>
@@ -38,5 +47,9 @@ loadSingleActivity()
 <style lang="scss" scoped>
 .el-header {
    background: linear-gradient(90deg,  #0775eb, #39ab2a);
+}
+a{
+   text-decoration: none;
+   color: #2c3e50;
 }
 </style>
