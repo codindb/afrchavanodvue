7205456a375f97d0f4a1061af4f7f02ce990be78
codindb <codindb.dev@gmail.com>
2021-09-09 14:02:46 +0200

    Refactio: mission style

--- src/pages/Mission.vue
+++ src/pages/Mission.vue
@@ -2,9 +2,8 @@
    <div class="mission" v-loading="apiData.isMissionLoading">
       <h2 v-if="apiData.mission">{{ apiData.mission.titre }}</h2>
       <el-image v-if="apiData.mission" :src="apiData.mission.photo.url" alt="logo AFR"></el-image>
-      <div v-if="apiData.mission" v-html="markdownToHtml(apiData.mission.description)"></div>
+      <div class="mission-description" v-if="apiData.mission" v-html="markdownToHtml(apiData.mission.description)"></div>
    </div>
-   <div class="bottomGap"></div>
 </template>
 
 <script setup>
@@ -22,6 +21,21 @@ const apiData = store.state.apiData
 
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+
+.mission {
+   min-height: 200px;
+   h2{
+   font-size: 36px;
+   }
+   .mission-description {
+      width: 96%;
+      margin: 0 auto;
+      ul{
+         display: inline-block !important;
+         text-align: left !important;
+      }
+   }
+}
 
 </style>
