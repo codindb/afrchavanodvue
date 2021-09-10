540596cd2a7e0749147b2495081fd90f886dc382
codindb <codindb.dev@gmail.com>
2021-09-10 10:27:28 +0200

    Add: store datas, mutations and actions

--- src/store/modules/data.js
+++ src/store/modules/data.js
@@ -4,6 +4,10 @@ export default {
      return {
        newsDialogVisibility: false,
        newsIndex: null,
+       drawer: false,
+       activityIndex: null,
+       workshopIndex: null,
+       kidsActivityIndex: null,
      }
    },
    mutations: {
@@ -15,11 +19,31 @@ export default {
       // NEWS INDEX
       SET_NEWS_INDEX (state, index) {
          state.newsIndex = index
-      }
+      },
+      
+      // ACTIVITY INDEX
+      SET_ACTIVITY_INDEX (state, index) {
+         state.activityIndex = index
+      },
+      
+      // WORKSHOP INDEX
+      SET_WORKSHOP_INDEX (state, index) {
+         state.workshopIndex = index
+      },
+      
+      // KIDS ACTIVITY INDEX
+      SET_KIDS_ACTIVITY_INDEX (state, index) {
+         state.kidsActivityIndex = index
+      },
+
+      // DRAWER
+      SET_DRAWER (state, boolean) {
+         state.drawer = boolean
+      },
     },
    actions: {
 
-      // DIALOG VISIBILITY
+      // NEWS DIALOG VISIBILITY
       setNewsDialogVisibility ({ commit }, boolean) {
          commit('SET_NEWS_DIALOG_VISIBILITY', boolean)
       },
@@ -27,6 +51,16 @@ export default {
       // NEWS INDEX
       setNewsIndex ({ commit }, index) {
          commit('SET_NEWS_INDEX', index)
+      },
+
+      // ACTIVITY INDEX
+      setActivityIndex ({ commit }, index) {
+         commit('SET_ACTIVITY_INDEX', index)
+      },
+
+      // DRAWER
+      setDrawer ({ commit }, boolean) {
+         commit('SET_DRAWER', boolean)
       },
    }
 }
