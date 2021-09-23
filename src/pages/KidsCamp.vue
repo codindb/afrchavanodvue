07618b57a58f9a5622735e8afa2fbcbbb1280352
codindb <codindb.dev@gmail.com>
2021-09-23 10:58:12 +0200

    Add: gallery carousel and dowload files section

--- src/pages/KidsCamp.vue
+++ src/pages/KidsCamp.vue
@@ -17,8 +17,17 @@
             <div v-if="apiData.kidsCamp" v-html="markdownToHtml(apiData.kidsCamp.tarif)"></div>
             <h3>Inscriptions</h3>
             <div v-if="apiData.kidsCamp" v-html="markdownToHtml(apiData.kidsCamp.inscriptions)"></div>
+            <h3 v-if="apiData.kidsCamp && apiData.kidsCamp.fichiers.length > 0">Documents à télécharger</h3>
+            <div class="files" v-if="apiData.kidsCamp && apiData.kidsCamp.fichiers.length > 0">
+               <el-button type="primary" plain round icon="el-icon-download" v-for="file in apiData.kidsCamp.fichiers" :key="file" @click="downloadFile(file.url, file.name)">{{ file.name }}</el-button>
+            </div>
             <h3>Informations supplémentaires</h3>
             <div v-if="apiData.kidsCamp" v-html="markdownToHtml(apiData.kidsCamp.info_supplementaire)"></div>
+            <el-carousel v-if="apiData.kidsCamp && apiData.kidsCamp.galerie.length > 0" height="300px" :interval="3000" indicator-position="outside" arrow="always" trigger="click">
+              <el-carousel-item v-for="(item, index) in apiData.kidsCamp.galerie" :key="item">
+                <el-image :src="item.formats && item.formats.medium ? item.formats.medium.url : item.url" style="width: 100%; height: 100%" fit="contain" alt="galerie d'image"></el-image>
+              </el-carousel-item>
+            </el-carousel>
             <h3>Contact</h3>
             <p v-if="apiData.kidsCamp"><a :href="'tel:+33' + apiData.kidsCamp.telephone">{{ apiData.kidsCamp.telephone}}</a></p>
             <p v-if="apiData.kidsCamp">{{ apiData.kidsCamp.email}}</p>
@@ -37,6 +46,14 @@
 
 import { useStore } from 'vuex'
 import { useRoute, useRouter } from 'vue-router'
+
+// To access the downloadFile function from firebase service
+import * as FirebaseStorageService from '../services/FirebaseStorageService.js'
+
+// Use firebase service to download file
+const downloadFile = (url, filename) => {
+   FirebaseStorageService.downloadFile(url, filename)
+}
 
 const store = useStore()
 const route = useRoute()
@@ -57,15 +74,15 @@ const apiData = store.state.apiData
    z-index: 0;
 
    h2 {
-   color: white;
-   font-size: 46px;
-   font-weight: bold;
-   margin-top: -20px;
+      color: white;
+      font-size: 46px;
+      font-weight: bold;
+      margin-top: -20px;
    }
    @media screen and (max-width: 768px) {
-   h2 {
+      h2 {
          font-size: 30px;
-   }
+      }
    }
 }
 .page-header {
@@ -97,12 +114,33 @@ const apiData = store.state.apiData
          max-width: 100%;
          width: 20%;
       }
+      .files {
+         width: 60%;
+         margin: 0 auto;
+         .el-button {
+            margin: 20px 15px;
+         }
+      }
+      .el-carousel {
+        width: 40%;
+        margin: 0 auto;
+        padding-top: 30px;
+      }
       @media screen and (max-width: 768px) {
          .hr {
             width: 30%;
          }
          .title-image {
             width: 60%;
+         }
+         .el-carousel {
+            width: 100%;
+         }
+         .files {
+            width: 100%;
+            .el-button {
+               margin: 10px;
+            }
          }
       }
    }
