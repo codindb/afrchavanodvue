ca9857d350ec5d34551b9970ac6e07cc239faba1
codindb <codindb.dev@gmail.com>
2022-08-12 16:23:34 +0200

    Change registration form for 2022

--- src/pages/Inscription.vue
+++ src/pages/Inscription.vue
@@ -1,5 +1,5 @@
 <template>
-   <iframe class="airtable-embed" src="https://airtable.com/embed/shrNFN4rwZzPf2V0u?backgroundColor=teal" frameborder="0" onmousewheel="" width="100%" height="533" style="background: transparent; border: 1px solid #ccc;"></iframe>
+   <iframe class="airtable-embed" src="https://airtable.com/embed/shrdEHoZffZHcRAOQ?backgroundColor=purple" frameborder="0" onmousewheel="" width="100%" height="533" style="background: transparent; border: 1px solid #ccc;"></iframe>
 </template>
 
 <script setup>
